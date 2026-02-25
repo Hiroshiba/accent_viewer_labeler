ce2fdcdf6436e3b4fd2e2869b1bea40f7b01e62e
Hiroshiba Kazuyuki <kazuyuki_hiroshiba@dwango.co.jp>
2026-02-25 19:31:46 +0900

    不要なコメント削除

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import App from "./App.vue";
 createApp(App)
   .mount("#app")
   .$nextTick(() => {
-    // Use contextBridge
     window.ipcRenderer.on("main-process-message", (_event, message) => {
       console.log(message);
     });
